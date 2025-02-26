8297e1d13b1691fd633f1b464f4fd3700fb22ebd
Espinac0 <davidgonncar@gmail.com>
2025-02-26 11:43:54 +0100

    pruebasjenkisnsaliomal

--- handler/pokemon_handler.py
+++ handler/pokemon_handler.py
@@ -1,16 +1,13 @@
 from models.pokemon_model import fetch_pokemon_by_id, fetch_pokemons_by_type, fetch_water_pokemons
 
 def get_pokemon_handler(pokemon_id: int) -> dict:
-    """Manejador que obtiene la información del Pokémon y la estructura."""
     pokemon_name = fetch_pokemon_by_id(pokemon_id)
     if not pokemon_name:
         return {"error": "Pokémon no encontrado"}
     return {"id": pokemon_id, "name": pokemon_name}
 
 def get_pokemons_by_type_handler(type_name: str) -> list:
-    """Manejador que obtiene Pokémon por tipo."""
     return fetch_pokemons_by_type(type_name)
 
 def get_water_pokemons_handler() -> list:
-    """Manejador para obtener Pokémon de tipo agua."""
     return fetch_water_pokemons()
--- view/view.py
+++ view/view.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from typing import List
-from models.response import Pokemon, PokemonName 
+from models.response import Pokemon
 from utils.Logger import Logger
 from handler.pokemon_handler import get_pokemon_handler, get_pokemons_by_type_handler, get_water_pokemons_handler
 
@@ -15,19 +15,19 @@ async def get_pokemon_by_id(id: int):
         raise HTTPException(status_code=404, detail="Pokémon no encontrado")
     return Pokemon(**pokemon)
 
-@router.get("/type/{type_name}", response_model=List[PokemonName])
+@router.get("/type/{type_name}", response_model=List[Pokemon])
 async def get_pokemon_by_type(type_name: str):
     pokemons = get_pokemons_by_type_handler(type_name)
     if not pokemons:
         logger.add_to_log("error", f"No se encontraron Pokémon del tipo {type_name}")
         raise HTTPException(status_code=404, detail=f"No se encontraron Pokémon del tipo {type_name}")
-    return [PokemonName(name=name) for name in pokemons]
-
-@router.get("/water-pokemons", response_model=List[PokemonName])
+    return [Pokemon(name=name) for name in pokemons]
+""""
+@router.get("/water-pokemons", response_model=List[Pokemon])
 async def get_water_pokemons():
     logger.add_to_log("info", "Solicitud recibida en /water-pokemons")
     pokemons = get_water_pokemons_handler()
     if not pokemons:
         logger.add_to_log("error", "No se encontraron Pokémon de tipo agua")
         raise HTTPException(status_code=404, detail="No se encontraron Pokémon de tipo agua")
-    return [PokemonName(name=name) for name in pokemons]
+    return [Pokemon(name=name) for name in pokemons]"""
